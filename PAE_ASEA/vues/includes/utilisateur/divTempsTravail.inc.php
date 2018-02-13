d69af58118c725acd311f8a92c3a42476ce77486
arichard44 <antoine.richard19@gmail.com>
2018-02-13 14:58:29 +0100

    tableau Temps Travail et CSS

--- PAE_ASEA/vues/includes/utilisateur/divTempsTravail.inc.php
+++ PAE_ASEA/vues/includes/utilisateur/divTempsTravail.inc.php
@@ -6,6 +6,96 @@
         <label>Offre d'emploi n° :</label>
         <input type="text" id="numOffreEmploi" required/>
         <br><br><br>
+        
+        <table class="tg">
+            <tr>
+                <th class="tg-uegx" colspan="7">Semaine 1</th>
+            </tr>
+            <tr>
+                <td class="tg-3rb4" rowspan="2">Périodes</td>
+                <td class="tg-3rb4" colspan="2">Matin</td>
+                <td class="tg-3rb4" colspan="2">Après-midi</td>
+                <td class="tg-3rb4" colspan="2">Nuit</td>
+            </tr>
+            <tr>
+                <td class="tg-24vz">Début</td>
+                <td class="tg-24vz">Fin</td>
+                <td class="tg-24vz">Début</td>
+                <td class="tg-24vz">Fin</td>
+                <td class="tg-24vz">Début</td>
+                <td class="tg-24vz">Fin</td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Lundi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Mardi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Mercredi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Jeudi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Vendredi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Samedi</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">Dimanche</td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+                <td class="tg-9wvm"><input type="time" name="cell1" id="cell1" step="300"/></td>
+            </tr>
+            <tr>
+                <td class="tg-3rb4">TOTAL Semaine</td>
+                <td class="tg-hnao" colspan="6"><input type="time" name="cell1" id="cell1" value="31:40" disabled="true" /></td>
+            </tr>
+        </table>
+        
+        <br><br><br>
+        
         <button id="BoutonSuivantDemande" type="submit">Suivant</button>
     </div>
 </form>
